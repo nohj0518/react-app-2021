24fde5cbeceae7963c88b472c35c1cb51af6da31
nohj0518 <38433755+nohj0518@users.noreply.github.com>
2021-05-24 20:36:45 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,60 @@
 import React, { Component } from "react";
-//import logo from "./logo.svg";
 import "./App.css";
+
+//Compoenet를 만들 때는 최상위 태그가 한 개 존재
+class Subject extends Component {
+  render() {
+    return (
+      <header>
+        <h1>{this.props.title}</h1>
+        {this.props.sub}
+      </header>
+    );
+  }
+}
+class TOC extends Component {
+  render() {
+    return (
+      <nav>
+        <ul>
+          <li>
+            <a href="1.html">HTML</a>
+          </li>
+          <li>
+            <a href="2.html">CSS</a>
+          </li>
+          <li>
+            <a href="3.html">Java</a>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+class Content extends Component {
+  render() {
+    return (
+      <article>
+        <h2>{this.props.title}</h2>
+        {this.props.desc}
+      </article>
+    );
+  }
+}
 
 class App extends Component {
   render() {
-    return <div className="App">Hello, React!</div>;
+    return (
+      <div className="App">
+        <Subject title="WEB" sub="world wide web!"></Subject>
+        <TOC></TOC>
+        <Content
+          title="HTML"
+          desc="HTML is HyperText Markup Language.🛠"
+        ></Content>
+      </div>
+    );
   }
 }
 
