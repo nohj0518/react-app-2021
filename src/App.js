2953d4b49b49de664de403660aa476019d3bf4d1
nohj0518 <38433755+nohj0518@users.noreply.github.com>
2021-05-29 17:35:41 +0900

    Add TOC lists
    from props data

--- src/App.js
+++ src/App.js
@@ -7,11 +7,30 @@ import "./App.css";
 //Compoenet를 만들 때는 최상위 태그가 한 개 존재
 
 class App extends Component {
+  //constructor는 render보다 먼저 실행되어 초기화를 담당한다
+  constructor(props) {
+    super(props);
+    this.state = {
+      subject: { title: "WEB", sub: "World Wide Web!" },
+      contents: [
+        { id: 1, title: "HTML", desc: "HTML is for information" },
+        { id: 2, title: "CSS", desc: "CSS is for design" },
+        {
+          id: 3,
+          title: "JavaScript",
+          desc: "JavaScript is for interactive",
+        },
+      ],
+    };
+  }
   render() {
     return (
       <div className="App">
-        <Subject title="WEB" sub="world wide web!"></Subject>
-        <TOC></TOC>
+        <Subject
+          title={this.state.subject.title}
+          sub={this.state.subject.sub}
+        ></Subject>
+        <TOC data={this.state.contents}></TOC>
         <Content
           title="HTML"
           desc="HTML is HyperText Markup Language.🛠"
