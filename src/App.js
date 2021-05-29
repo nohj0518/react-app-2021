981a89b8eb22517977be1c760eaf3431f904cc32
nohj0518 <38433755+nohj0518@users.noreply.github.com>
2021-05-29 18:00:44 +0900

    Add state mode
    welcome mode, read mode

--- src/App.js
+++ src/App.js
@@ -11,7 +11,9 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      mode: "welcome",
       subject: { title: "WEB", sub: "World Wide Web!" },
+      welcome: { title: "Welcome", desc: "Hello, React!!" },
       contents: [
         { id: 1, title: "HTML", desc: "HTML is for information" },
         { id: 2, title: "CSS", desc: "CSS is for design" },
@@ -24,6 +26,16 @@ class App extends Component {
     };
   }
   render() {
+    var _title,
+      _desc = null;
+    if (this.state.mode === "welcome") {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if (this.state.mode === "read") {
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
+
     return (
       <div className="App">
         <Subject
@@ -31,10 +43,7 @@ class App extends Component {
           sub={this.state.subject.sub}
         ></Subject>
         <TOC data={this.state.contents}></TOC>
-        <Content
-          title="HTML"
-          desc="HTML is HyperText Markup Language.🛠"
-        ></Content>
+        <Content title={_title} desc={_desc}></Content>
       </div>
     );
   }
