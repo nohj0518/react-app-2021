f92988cc6f4787e93b20bcab9c9852afd38363b9
nohj0518 <38433755+nohj0518@users.noreply.github.com>
2021-06-01 00:55:14 +0900

    Create Control

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import TOC from "./components/TOC";
 import Content from "./components/Content";
 import Subject from "./components/Subject";
+import Control from "./components/Control";
 import "./App.css";
 
 //Compoenet를 만들 때는 최상위 태그가 한 개 존재
@@ -70,6 +71,13 @@ class App extends Component {
           }.bind(this)}
           data={this.state.contents}
         ></TOC>
+        <Control
+          onChangeMode={function (_mode) {
+            this.setState({
+              mode: _mode,
+            });
+          }.bind(this)}
+        ></Control>
         <Content title={_title} desc={_desc}></Content>
       </div>
     );
