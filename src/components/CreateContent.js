36fa03cd945f4b1af807b9c26836f71199e8d5dd
nohj0518 <38433755+nohj0518@users.noreply.github.com>
2021-06-02 22:37:09 +0900

    Make Form part

--- src/components/CreateContent.js
+++ src/components/CreateContent.js
@@ -4,7 +4,26 @@ class CreateContent extends Component {
     return (
       <article>
         <h2>Create</h2>
-        <form></form>
+        <form
+          action="/create_process"
+          method="post"
+          onSubmit={function (e) {
+            e.preventDefault(); /*
+          preventDefault()는 submit버튼을 눌렀을때 /create_process로 
+          페이지가 넘어가는 것을 막아 준다 */
+            alert("Submit!");
+          }.bind(this)}
+        >
+          <p>
+            <input type="text" name="title" placeholder="title"></input>
+          </p>
+          <p>
+            <textarea name="desc" placeholder="description"></textarea>
+          </p>
+          <p>
+            <input type="submit"></input>
+          </p>
+        </form>
       </article>
     );
   }
